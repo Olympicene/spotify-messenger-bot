cb1c2b251cf6dbb716a44827434e09d8ffc10358
Olympicene <kevinjcord@gmail.com>
2022-09-26 23:58:40 -0500

    added non song detection message

--- src/AddToPlaylist.js
+++ src/AddToPlaylist.js
@@ -22,72 +22,83 @@ async function addToPlaylist(event) {
     let message = {};
     const url = new URL(event.body);
     const id = url.pathname.split('/').at(-1);
+    const path_type = url.pathname.split('/').at(1);
 
     if (url.hostname == 'open.spotify.com' && id.length == 22) {
-      if (!Timeout.inTimeout(event.senderID)) {
-        try {
-          const spotifyApi = await getSpotifyToken();
-          await spotifyApi.addTracksToPlaylist(config.playlist_ID, [
-            `spotify:track:${id}`,
-          ]);
-
-          let info = await spotifyApi.getTracks([id]);
-          let track = info.body.tracks[0];
-
-          if (config.DEBUG) console.log(track);
-
-          //https://stackoverflow.com/a/46694321 create list of sub properties
-          let artists = track.artists.map((a) => a.name).join(', ');
-
-          message.body = `Added \"${track.name}\" by ${artists}`;
-
+      if (path_type == 'track') {
+        if (!Timeout.inTimeout(event.senderID)) {
           try {
-            let image = track.album.images[1].url;
+            const spotifyApi = await getSpotifyToken();
+            await spotifyApi.addTracksToPlaylist(config.playlist_ID, [
+              `spotify:track:${id}`,
+            ]);
+
+            let info = await spotifyApi.getTracks([id]);
+            let track = info.body.tracks[0];
+
+            if (config.DEBUG) console.log(track);
+
+            //https://stackoverflow.com/a/46694321 create list of sub properties
+            let artists = track.artists.map((a) => a.name).join(', ');
+
+            message.body = `Added \"${track.name}\" by ${artists}`;
 
             try {
-              fs.unlinkSync(`${appRoot}/image.png`);
-            } catch (error) {}
+              let image = track.album.images[1].url;
 
-            fs.writeFileSync(`${appRoot}/image.png`, await download(image), {
-              flag: 'w',
-            });
+              try {
+                fs.unlinkSync(`${appRoot}/image.png`);
+              } catch (error) {}
 
-            message.attachment = [fs.createReadStream(`${appRoot}/image.png`)];
-            await send(message, event.threadID, event.messageID);
+              fs.writeFileSync(`${appRoot}/image.png`, await download(image), {
+                flag: 'w',
+              });
+
+              message.attachment = [
+                fs.createReadStream(`${appRoot}/image.png`),
+              ];
+              await send(message, event.threadID, event.messageID);
+            } catch (error) {
+              console.log(error);
+            }
+
+            Timeout.userTimeout(event.senderID);
+
+            try {
+              message = {};
+              let audio = track.preview_url;
+
+              try {
+                fs.unlinkSync(`${appRoot}/audio.mp3`);
+              } catch (error) {}
+              fs.writeFileSync(`${appRoot}/audio.mp3`, await download(audio), {
+                flag: 'w',
+              });
+
+              message.attachment = [
+                fs.createReadStream(`${appRoot}/audio.mp3`),
+              ];
+              await send(message, event.threadID);
+            } catch (error) {
+              console.log(error);
+            }
           } catch (error) {
-            console.log(error);
-          }
-
-          Timeout.userTimeout(event.senderID);
-
-          try {
             message = {};
-            let audio = track.preview_url;
-
-            try {
-              fs.unlinkSync(`${appRoot}/audio.mp3`);
-            } catch (error) {}
-            fs.writeFileSync(`${appRoot}/audio.mp3`, await download(audio), {
-              flag: 'w',
-            });
-
-            message.attachment = [fs.createReadStream(`${appRoot}/audio.mp3`)];
-            await send(message, event.threadID);
-          } catch (error) {
-            console.log(error);
+            message.body =
+              'There was an error adding your song. Please check the logs.';
+            send(message, event.threadID, event.messageID);
+            console.error(error);
           }
-        } catch (error) {
+        } else {
           message = {};
-          message.body =
-            'There was an error adding your song. Please check the logs.';
+          message.body = `Sorry you're still in timeout, you can add a new song in ${Timeout.timeLeft(
+            event.senderID
+          )}`;
           send(message, event.threadID, event.messageID);
-          console.error(error);
         }
       } else {
         message = {};
-        message.body = `Sorry you're still in timeout, you can add a new song in ${Timeout.timeLeft(
-          event.senderID
-        )}`;
+        message.body = `We can't accept this ${path_type}.`;
         send(message, event.threadID, event.messageID);
       }
     }
